c1d0035f493131b136fabd337eaedc2ca71cbf85
Anastasia <anstice.a@gmail.com>
2022-08-11 19:31:07 +0300

    Task3

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Написать программу, которая на вход принимает число и выдает, является ли число четным
+
+int Prompt(string message) 
+{
+    System.Console.Write(message);      //Вывод сообщения
+    string value = Console.ReadLine();  //Считывание с консоли строки
+    int result = int.Parse (value);     //Преобразование строки в целое число
+    return result;                      //Возврат результата
+}
+
+int a;
+a = Prompt ("Введите число: ");
+
+if ((a % 2) == 0) 
+{
+    System.Console.WriteLine ($"Число четное");
+}
+else 
+{
+   System.Console.WriteLine ($"Число нечетное");
+}
