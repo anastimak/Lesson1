f16cdaaee3b63fd0b2ab368612400a302e5b616c
Anastasia <anstice.a@gmail.com>
2022-08-11 19:20:44 +0300

    Task2

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая на вход принимает три числа и выдает максимальное из этих чисел
+
+int Prompt(string message) 
+{
+    System.Console.Write(message);      //Вывод сообщения
+    string value = Console.ReadLine();  //Считывание с консоли строки
+    int result = int.Parse (value);     //Преобразование строки в целое число
+    return result;                      //Возврат результата
+}
+
+int a;
+int b;
+int c;
+a = Prompt ("Введите первое число: ");
+b = Prompt ("Введите второе число: ");
+c = Prompt ("Введите третье число: ");
+
+int max = a;
+if (max < b) {max = b;}
+if (max < c) {max = c;}
+
+System.Console.WriteLine ($"Максимальное число - {max}");
