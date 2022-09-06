708d7df5c853582804727116dfb4d510df9b91f8
Anastasia <anstice.a@gmail.com>
2022-09-06 16:52:10 +0300

    Homework

---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223 -> 3
+
+int Prompt(string message) 
+{
+    System.Console.Write(message);      //Вывод сообщения
+    string value = Console.ReadLine();  //Считывание с консоли строки
+    int result = int.Parse (value);     //Преобразование строки в целое число
+    return result;                      //Возврат результата
+}
+
+int [] inputArray (int number)     
+{
+    int [] array = new int [number];
+    int i = 0;
+    while (i < number)
+    {
+        System.Console.WriteLine ("Введите число: ");
+        array [i] = int.Parse (Console.ReadLine());
+        i++;
+    }
+    return array;
+}
+
+void printArray (int [] array)          //Вывод массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write (array [i] + " ");
+    }
+}
+
+int countPositiveNum (int [] array)    //Счетчик положительных чисел
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int [] array = inputArray (Prompt ("Укажите количество цифр, которые хотите ввести: "));
+printArray (array);
+System.Console.Write ("");
+System.Console.WriteLine ($"Количество введенных чисел после 0 равно: {countPositiveNum(array)}");
---
+++ Lesson6/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Prompt(string message) 
+{
+    System.Console.Write(message);      //Вывод сообщения
+    string value = Console.ReadLine();  //Считывание с консоли строки
+    int result = int.Parse (value);     //Преобразование строки в целое число
+    return result;                      //Возврат результата
+}
+
+int k1 = Prompt ("Введите k1: ");
+int b1 = Prompt ("Введите b1: ");
+int k2 = Prompt ("Введите k2: ");
+int b2 = Prompt ("Введите b2: ");
+
+void intersectiounOfLines (int k1, int b1, int k2, int b2)
+{
+    double x = (b1 - b2) / (k1 - k2);
+    double y = k1 * x + b1;
+    System.Console.WriteLine ($"Точка пересечения: {x}; {y}");
+}
+
+intersectiounOfLines (k1, b1, k2, b2);
